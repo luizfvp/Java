ca2f4c75022ce9eeb777ea8dd859da0d82e86da6
luizfvp <luizfvp@hotmail.com>
2025-06-16 14:40:00 -0300

    Questão atualizada

--- 1°Semestre 1.2025/AtivFinal_LuizFelipeVieira/src/Questao.java
+++ 1°Semestre 1.2025/AtivFinal_LuizFelipeVieira/src/Questao.java
@@ -10,34 +10,41 @@ public class Questao {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
         
-        String[] setor = new String[6]; //nome do setor tipo String
-        int[][] funcionarios = new int[6][2]; //matriz bidimensional tipo int, com número de setores e coluna para homens e mulheres
-        double[] porcentagemMulheres = new double[6]; //porcentagem de mulheres de cada setor
+        String[] setor = new String[3]; //nome do setor tipo String
+        int[][] funcionarios = new int[setor.length][2]; //matriz bidimensional tipo int, com número de setores e coluna para homens e mulheres
+        double[] porcentagemMulheres = new double[setor.length]; //porcentagem de mulheres de cada setor
 
         for (int i = 0; i < setor.length; i++) {
             System.out.println("\nDigite o nome do " + (i + 1) + "° setor: ");
             setor[i] = leia.nextLine(); //receber o nome do setor no vetor
 
-            System.out.println("\nDigite o número de funcionarios homens do " + (i + 1) + "° setor: ");
+            System.out.println("Digite o número de funcionarios homens do " + (i + 1) + "° setor: ");
             funcionarios[i][0] = leia.nextInt(); //receber na primeira coluna da matriz bidimensional o número de homens
 
-            System.out.println("\nDigite o número de funcionarias mulheres do " + (i + 1) + "° setor: ");
+            System.out.println("Digite o número de funcionarias mulheres do " + (i + 1) + "° setor: ");
             funcionarios[i][1] = leia.nextInt(); //receber na segunda coluna da matriz bidimensional o número de mulheres
             leia.nextLine();
 
             int totalProvisorio = funcionarios[i][0] + funcionarios[i][1]; //calculando a soma de homens com mulheres
             double totalFuncionarios = totalProvisorio; //convertendo o total, que era tipo Int, em tipo double
 
-            if (funcionarios[i][1] != 0) {
-                porcentagemMulheres[i] = (funcionarios[i][1] / totalFuncionarios) * 100; //calculando a porcentagem de mulheres
-            } else {
-                porcentagemMulheres[i] = 0;
-            }
+            porcentagemMulheres[i] = (funcionarios[i][1] / totalFuncionarios) * 100; //calculando a porcentagem de mulheres
         }
 
+        System.out.println("\n------------------------------------------------------");
+        System.out.println("Relatório de Funcionários por Setor: ");
         for (int i = 0; i < setor.length; i++) {
             System.out.printf("\nNome do Setor: %s\nNúmero de Homens: %s\nNúmero de Mulheres: %s\nPercentual de Mulheres: %.1f%%\n", setor[i], funcionarios[i][0], funcionarios[i][1], porcentagemMulheres[i]);
-            System.out.println( "--------------------------------");
+        }
+
+        System.out.println("\n------------------------------------------------------");
+        System.out.println("Setores com menos de 30% de Mulheres: ");
+        for (int i = 0; i < setor.length; i++) {
+            if (porcentagemMulheres[i] < 30) {
+                System.out.printf("\nNome do Setor: %s\nPercentual de Mulheres: %.1f%%\n", setor[i], porcentagemMulheres[i]);
+            } else {
+                System.out.println("\nNão há setor com menos de 30% de mulheres");
+            }
         }
 
         leia.close();
